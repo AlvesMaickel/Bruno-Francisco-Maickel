a4963403622db7dce92df3e71667414d978164b6
francisco212 <francisco_neto_96@hotmail.com>
2014-09-25 00:50:25 -0300

    validacoes adicionadas e testadas #16

--- forum/app/models/lista.rb
+++ forum/app/models/lista.rb
@@ -1,3 +1,10 @@
 class Lista < ActiveRecord::Base
   belongs_to :conteudo
+  validates :arquivo,uniqueness: true
+
+  validates :nome,presence: true
+
+  validates :conteudo,presence: true
+
+  validates :arquivo,presence: true
 end
