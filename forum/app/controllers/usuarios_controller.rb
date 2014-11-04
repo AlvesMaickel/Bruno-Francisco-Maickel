58488198fc6e80ece5635ead1bcb5bc29be3bd28
francisco212 <francisco_neto_96@hotmail.com>
2014-11-03 22:10:01 -0200

    view e metodo  criados #49

--- forum/app/controllers/usuarios_controller.rb
+++ forum/app/controllers/usuarios_controller.rb
@@ -1,7 +1,26 @@
+#Encoding UTF-8
 class UsuariosController < ApplicationController
    def index
    	@usuarios=Usuario.all
    end
 
+   def new
+        @usuario = Usuario.new
+   end
+
+
+   def create
+   
+
+          @usuario = Usuario.new(params.require(:usuario).permit(:username,:email,:senha))
+          @usuario.tipo=Tipo.find_by_id(1)        
+          if @usuario.save
+              redirect_to :usuarios, notice: "Usuario #{@usuario.username} salvo"
+          else
+              render :new
+          end
+      
+
+    end
 
 end
