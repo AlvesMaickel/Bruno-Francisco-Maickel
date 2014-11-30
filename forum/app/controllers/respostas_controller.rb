2b2c37f0917ed6e3e823f45475b53e2f98e34288
francisco212 <francisco_neto_96@hotmail.com>
2014-11-29 23:21:41 -0200

    controller modificado #88

--- forum/app/controllers/respostas_controller.rb
+++ forum/app/controllers/respostas_controller.rb
@@ -9,7 +9,10 @@ class RespostasController < ApplicationController
 			@resposta.pergunta=Pergunta.find(session[:pergunta])
             @resposta.util_n=0
             @resposta.util_s=0
-            @pergunta=Pergunta.find(session[:pergunta])		
+            @pergunta=Pergunta.find(session[:pergunta])	
+            n=@pergunta.n_respostas + 1
+            @pergunta.update_attribute(:respondida,'s')            
+            @pergunta.update_attribute(:n_respostas,n)
  		if @resposta.save
 			
 			redirect_to "/perguntas/#{@pergunta.id}", notice: "resposta #{@resposta.texto} realizada com sucesso"
